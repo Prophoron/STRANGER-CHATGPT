10a19c00e2c9082f0fc80a822e4def341418a16b
Prophoron <Phoron93@proton.me>
2024-10-01 19:16:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return ' I Am alive'
+def home():
+    return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
